c64f5f2e5ea870f631d7e4ddef164699bb264c1e
rjhallsted <rjhallsted@gmail.com>
2016-06-08 13:23:22 -0600

    Changed custom field sanitization functions to esc_raw_url.

--- includes/custom-fields.php
+++ includes/custom-fields.php
@@ -4,15 +4,10 @@ require_once get_template_directory()  . '/includes/class-rjh-simple-custom-fiel
 
 function rjh_project_link_meta() {
 	global $project_link_meta;
-	$project_link_meta = new RJHSimpleCustomField( 'Project Link', 'project', 'normal', 'default', 'rjh_sanitize_project_link');
+	$project_link_meta = new RJHSimpleCustomField( 'Project Link', 'project', 'normal', 'default', 'esc_raw_url');
 	$project_link_meta->init();
 }
 add_action( 'init', 'rjh_project_link_meta' );
-
-function rjh_sanitize_project_link( $value ) {
-	return esc_url_raw( $value );
-}
-
 function rjh_get_project_link( $post_id ) {
 	global $project_link_meta;
 	$raw_url = $project_link_meta->get_meta( $post_id );
@@ -21,14 +16,10 @@ function rjh_get_project_link( $post_id ) {
 
 function rjh_writing_link_url_meta() {
 	global $writing_link_url_meta;
-	$writing_link_url_meta = new RJHSimpleCustomField( 'Link URL', 'writing-link', 'normal', 'default', 'rjh_sanitize_writing_link_url');
+	$writing_link_url_meta = new RJHSimpleCustomField( 'Link URL', 'writing-link', 'normal', 'default', 'esc_raw_url');
 	$writing_link_url_meta->init();
 }
 add_action( 'init', 'rjh_writing_link_url_meta' );
-
-function rjh_sanitize_writing_link_url( $value ) {
-	return esc_url_raw( $value );
-}
 
 function rjh_get_writing_link_url( $post_id ) {
 	global $writing_link_url_meta;
